4220ba2512b63a930733b9b5e67bea5f7e61b578
tunamagu <tkshntr@yahoo.co.jp>
2019-10-17 16:21:51 +0000

    update_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/new'
-  get 'tasks/create'
-  get 'tasks/show'
-  get 'tasks/edit'
-  get 'tasks/update'
-  get 'tasks/destroy'
   devise_for :users
+  resources :tasks
+
+  root 'tasks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
