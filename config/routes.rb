d29d3d39b17e0ba0beb8df496f8b32e61cf2a037
tunamagu <tkshntr@yahoo.co.jp>
2019-10-17 08:51:37 +0000

    add_tasks_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/new'
+  get 'tasks/create'
+  get 'tasks/show'
+  get 'tasks/edit'
+  get 'tasks/update'
+  get 'tasks/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
