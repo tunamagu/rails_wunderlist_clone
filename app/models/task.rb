dc82b7997969c1aac08e5a79e08c6582410f46cb
tunamagu <tkshntr@yahoo.co.jp>
2019-10-18 05:47:07 +0000

    update_models

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+  belongs_to :user
+
+  # default_scope -> { order(created_at: :desk)}
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :memo, length: { maximum: 2048 }
 end
