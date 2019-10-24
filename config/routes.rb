d7d51fc730e0c9736a6f60c7558d7eebe2b084a9
tunamagu <tkshntr@yahoo.co.jp>
2019-10-24 07:37:03 +0000

    add_image_delete

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   root 'tasks#index'
   get 'tasks/done/:id', to: 'tasks#done', as: 'done_task'
+  delete 'tasks/image_delete/:id', to: 'tasks#image_delete', as: 'image_delete'
 end
